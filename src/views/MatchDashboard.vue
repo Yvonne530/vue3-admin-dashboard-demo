d4fc5ae6aa87479f1e7ac25c15d0219951d41012
简健怡 <jianyijian693@gmail.com>
2025-08-04 18:07:57 +0800

    Update MatchDashboard.vue

---
+++ src/views/MatchDashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="min-h-screen bg-gray-100 p-6">
+    <div class="max-w-7xl mx-auto">
+      <h1 class="text-2xl font-bold mb-6">智能营销配置平台</h1>
+      <div class="flex flex-col lg:flex-row gap-6">
+        <ManualSelectPanel />
+        <AiMatchPanel />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ManualSelectPanel from '@/components/ManualSelectPanel.vue'
+import AiMatchPanel from '@/components/AiMatchPanel.vue'
+</script>

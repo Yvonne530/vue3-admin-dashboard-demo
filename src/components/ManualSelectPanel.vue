4cb0fa9396f0e0e33cd5c4a61f2a22521ad1b7bb
简健怡 <jianyijian693@gmail.com>
2025-08-04 17:58:36 +0800

    Update ManualSelectPanel.vue

---
+++ src/components/ManualSelectPanel.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full lg:w-1/2 pr-4">
+    <StoreInfoCard />
+    <GoodsGroupCard />
+    <CustomerGroupCard />
+  </div>
+</template>
+
+<script setup>
+import StoreInfoCard from './StoreInfoCard.vue'
+import GoodsGroupCard from './GoodsGroupCard.vue'
+import CustomerGroupCard from './CustomerGroupCard.vue'
+</script>
